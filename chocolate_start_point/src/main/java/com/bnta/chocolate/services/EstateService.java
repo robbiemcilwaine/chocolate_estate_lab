0c89b7fa31833e8f12732d2b41ecbb9d4fdaf529
Robbie Mcilwaine <robbiemcilwaine99@gmail.com>
2024-06-04 16:53:43 +0100

    wrote method for returning estate objects

--- chocolate_start_point/src/main/java/com/bnta/chocolate/services/EstateService.java
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/services/EstateService.java
@@ -5,6 +5,8 @@ import com.bnta.chocolate.repositories.EstateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EstateService {
 
@@ -15,5 +17,9 @@ public class EstateService {
         return estateRepository.save(estate);
 //        return estate;
     }
-    
+
+    public List<Estate> getAllEstates(){
+        return estateRepository.findAll();
+    }
+
 }
